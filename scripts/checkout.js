461e73e5560bcea3ec161282cb2fd9e415e9a199
Max <mugunamaxwel@gmail.com>
2025-05-08 10:47:56 +0300

    Convert code to a promise that loads products

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,13 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js"; 
 
-loadProducts(() => {
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();//Lets us control when to go to the next step
+  });
+
+}).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-});
+})
