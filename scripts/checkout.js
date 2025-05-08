7bcb8cd3a73a9d09497cf0866e8f74168a9ff663
Max <mugunamaxwel@gmail.com>
2025-05-08 11:10:24 +0300

    Use fetch to load products using loadProductsFetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,14 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js"; 
+import { loadProductsFetch } from "../data/products.js"; 
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();//Lets us control when to go to the next step
-  });
-
-}).then(() => {
+loadProductsFetch().then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
